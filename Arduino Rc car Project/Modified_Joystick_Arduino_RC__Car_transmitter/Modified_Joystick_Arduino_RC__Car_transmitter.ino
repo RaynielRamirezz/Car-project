914ea8a659891032c5d9ad3bf02a65c8594b1455
RaynielRamirezz <59105007+RaynielRamirezz@users.noreply.github.com>
2020-01-22 12:05:54 -0400

    Add files via upload

---
+++ Arduino Rc car Project/Modified_Joystick_Arduino_RC__Car_transmitter/Modified_Joystick_Arduino_RC__Car_transmitter.ino
@@ -0,0 +1,145 @@
+#include <SPI.H>
+#include <RH_NRF24.h>
+#include <RHReliableDatagram.h>
+
+#define CLIENT 1
+#define SERVER 2
+
+RH_NRF24 radios;
+
+RHReliableDatagram radio(radios,CLIENT);
+
+#define joyhorz  A3 // analog pin connected to x output
+#define joyvert  A4 // analog pin connected to y output
+
+int joyVert = 512;
+int joyHorz = 512;
+
+uint8_t motor[3];
+
+//definemessage buffer
+uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+
+//int receiver = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+
+Serial.begin(9600);
+
+if(!radio_manager.init())
+  Serial.println("init failed");
+
+  motor[2] = 0;//forward
+
+}
+
+void loop() {
+
+Serial.println("motor values ");
+
+joyHorz = analogRead(joyhorz);
+joyVert = analogRead(joyvert);
+
+if(joyVert < 453){
+  
+  motor[2] = 1;//backward;
+
+  //motor speeds(determining)
+ //as we're going backward we need to reverse reading
+    motor[0] = map(joyVert,453,0,0,255);
+    motor[1] = map(joyVert,453,0,0,255);
+
+  
+  }  else if(joyvert > 558){
+    
+    motor[2] = 0;//foward
+    
+    //determine motor speeds
+
+    motor[0] = map(joyVert,558,1023,0,255);
+    motor[1] = map(joyVert,558,1023,0,255);
+    
+    
+    }else
+
+    {
+      //stop motors from running
+     motor[0] = 0;
+     motor[1] = 0;
+     motor[2] = 0;
+    }
+
+     if(joyHorz < 453){
+ 
+ 
+    joyHorz = map(joyHorz,453,0,0,255);
+
+    motor[0] -= joyHorz;//decreases left motor
+    motor[1] += joyHorz;//increases right motor
+
+    //dont exceed range of 0-255 for motor speeds
+    if(motor[1] > 255)
+    motor[1] = 255;
+
+    if(motor[0] < 0)
+    motor[0] = 0;
+    
+      
+     }else if (joyHorz > 558){
+ /* move right. 
+ Map the number to a value of 255 maximum*/
+ 
+    joyHorz = map(joyHorz,558,1023,0,255);
+
+    motor[0] += joyHorz;//increase left motor
+    motor[1] -= joyHorz;//decrease right motor
+
+    //dont exceed range of 0-255 for motor speeds
+    if(motor[1] < 0)
+    motor[1] = 0;
+
+    if(motor[0] > 255)
+    motor[0] = 255;
+
+}
+
+/*Serial.print("motor A: ");
+Serial.print(motor[0]);
+Serial.print("motor B: ");
+Serial.print(motor[1]);
+Serial.print("Direction ");
+Serial.print(motor[2]);
+analogWrite(EnA,motor[1]);
+analogWrite(EnB,motor[0]);
+if(joyVert < 453){
+  
+  motorcontrol[2] = 1;//backward;
+
+  //motor speeds(determining)
+ //as we're going backward we need to reverse reading
+    motor[0] = map(joyVert,453,0,0,255);
+    motor[1] = map(joyVert,453,0,0,255);
+
+  
+  }  else if(joyvert > 558){
+    
+    motor[2] = 0;//foward
+    
+    //determine motor speeds
+
+    motor[0] = map(joyVert,558,1023,0,255);
+    motor[1] = map(joyVert,558,1023,0,255);
+    
+    
+    }*/
+
+if (radio.sendtoWait(motor,sizeof(motorcontrol),SERVER)){
+
+Serial.println("Done");
+
+     delay(150);// wait a bit before next transmission*/
+
+}
+   
+
---
+++ Arduino Rc car Project/modified_receiver_remote_control_car/modified_receiver_remote_control_car.ino
@@ -0,0 +1,84 @@
+/*analogWrite(EnA,motorcontrol[1]);
+analogWrite(EnB,motorcontrol[0]);*/
+
+
+/*if(motorcontrol[2] == 1){
+ digitalWrite(In1,HIGH);
+ digitalWrite(In2,LOW);
+ digitalWrite(In3,HIGH);
+ digitalWrite(In4,LOW);
+  
+}else if (motorcontrol[2] == 0){
+ digitalWrite(In1,LOW);
+ digitalWrite(In2,HIGH);
+ digitalWrite(In3,LOW);
+ digitalWrite(In4,HIGH);
+
+}
+*/
+
+#include <SPI.h>
+#include <RH_NRF24.h>
+#include <RHReliableDatagram.h>
+#define SERVER 2
+#define CLIENT 1   
+
+ 
+int INB = 4;
+int IN3 = 5;//connected to analog pins
+int IN4 = 9;
+int INA = 12;
+int IN1 = 7;//connected to analog pins
+int IN2 = 2;
+ 
+
+
+
+RH_NRF24 radios;
+uint8_t info[RH_NRF24_MAX_MESSAGE_LEN];
+RHReliableDatagram radio(radios, SERVER);
+ 
+
+void setup()
+{
+  radio.init();
+    
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(INA, OUTPUT);
+  pinMode(INB, OUTPUT); } 
+ 
+void loop(){
+  if (radio.available()){
+    uint8_t frominfo;
+    uint8_t lengths = sizeof(info);
+    
+    if (radio.recvfromAck(info, &lengths, &frominfo)){
+ 
+      if (info[2] == 1)
+      {
+
+    //  backwards
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH); }
+    
+    else if (info[2] == 0){
+    // forwards
+
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);}
+ 
+      
+      // drive motors
+      analogWrite(INB, info[0]);
+      analogWrite(INA, info[1]);
+      
+      }
+  }              
+}
